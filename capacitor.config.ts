e513bbf55b4c3fd5b8d2d9218d84dc0dea3eec87
SunaAyhan <sunaayhan16@gmail.com>
2024-04-27 02:05:49 +0300

    Frontend bugs fixed for mobil.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ionic.evisitor',
-  appName: 'evisitor',
+  appId: 'sunaayhan.guestbook',
+  appName: 'Louvre GuestBook',
   webDir: 'dist'
 };
 
