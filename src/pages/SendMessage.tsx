be4817e7aa83e04318d8fc29389062a4af0f8f2b
SunaAyhan <sunaayhan16@gmail.com>
2024-04-26 13:18:44 +0300

    Remove ignored files

--- src/pages/SendMessage.tsx
+++ src/pages/SendMessage.tsx
@@ -1,7 +1,6 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
 import TextArea from '../components/TextArea';
 import ReviewPage from '../components/Reviews';
 
